8c077eb31ba3e8d5d6dd72309a76d5c36a8e7a44
Ahmed R. Mazen <ahmed.mazen87@yahoo.com>
2022-11-04 21:46:31 +0300

    comment security

--- src/main/java/in/co/gorest/GorestappApplication.java
+++ src/main/java/in/co/gorest/GorestappApplication.java
@@ -3,9 +3,7 @@ package in.co.gorest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class GorestappApplication {
--- src/main/java/in/co/gorest/config/OAuth2ResourceServerConfig.java
+++ src/main/java/in/co/gorest/config/OAuth2ResourceServerConfig.java
@@ -1,17 +1,17 @@
 package in.co.gorest.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
-@Configuration
-public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
-
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/gorestapp/**").authenticated()
-//                .antMatchers("/").permitAll();
-//    }
-}
+//@Configuration
+//public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+//
+////    @Override
+////    public void configure(HttpSecurity http) throws Exception {
+////        http
+////                .authorizeRequests()
+////                .antMatchers("/gorestapp/**").authenticated()
+////                .antMatchers("/").permitAll();
+////    }
+//}
